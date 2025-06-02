8369fa5c650d3ae19f73b6f96a4e8fcdbe31f7c9
ahedmafarjeh <ahedmafarjeh10@gmail.com>
2025-06-02 21:19:40 +0300

    product mvc with database

---
+++ product_task1/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using product_task1.Data;
+
+namespace product_task1.Controllers
+{
+    public class ProductController : Controller
+    {
+        ApplicationDbContextcs context = new ApplicationDbContextcs();
+        public ViewResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+        public ViewResult Details()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+        public ViewResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ product_task1/Data/ApplicationDbContextcs.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using product_task1.Models;
+
+namespace product_task1.Data
+{
+    public class ApplicationDbContextcs:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=T1MVC_11;TrustServerCertificate=True;Trusted_Connection=True");
+        }
+    }
+}
